efce4dac90ed0bf985a69be537c8f24becc545c1
Cristhian Burbano <cristhian_burbano@hotmail.com>
2020-09-06 18:09:39 -0500

    Actualiacion de Facultad v1

--- src/java/org/Object/Facultad.java
+++ src/java/org/Object/Facultad.java
@@ -7,27 +7,27 @@ package org.Object;
 
 /**
  *
- * @author F
+ * @author crist
  */
 public class Facultad {
     
-    Integer id;
+    Integer id_facultad;
     String nombre;
 
     public Facultad() {
     }
 
-    public Facultad(Integer id, String nombre) {
-        this.id = id;
+    public Facultad(Integer id_facultad, String nombre) {
+        this.id_facultad = id_facultad;
         this.nombre = nombre;
     }
 
-    public Integer getId() {
-        return id;
+    public Integer getId_facultad() {
+        return id_facultad;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setId_facultad(Integer id_facultad) {
+        this.id_facultad = id_facultad;
     }
 
     public String getNombre() {
@@ -37,5 +37,6 @@ public class Facultad {
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+    
     
 }
